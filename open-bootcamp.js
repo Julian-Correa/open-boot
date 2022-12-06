97e47a9f302e9ac49945a78f591a4e1aba2d8fb1
dessinger <108768144+Julian-Correa@users.noreply.github.com>
2022-12-06 07:41:09 -0300

    Add files via upload

---
+++ open-bootcamp.js
@@ -0,0 +1,9 @@
+const alumno  = ["Julián" , 20, false,
+ libro = {
+  título:"padre Rico padre pobre",
+  autor: "kiyosaki",
+  fecha: 1997,
+  url :" https://es.m.wikipedia.org/wiki/Padre_Rico,_Padre_Pobre "
+ } ]
+
+ console.log(alumno)
